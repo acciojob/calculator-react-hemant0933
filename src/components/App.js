17106d8802e92807842af0150faea5933f7d22ed
hemant0933 <116268777+hemant0933@users.noreply.github.com>
2023-04-13 10:56:29 +0530

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,2 +1,57 @@
-<p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
 
+import  React, {useState} from "react";
+
+const App = () => {
+    let [inputText, setInputText] = useState("");
+
+    const buttonClick = (event) => {
+        let clickedButton = event.target;
+        let value = clickedButton.innerHTML;
+        setInputText(inputText + value);
+
+
+
+    }
+
+
+    const ans = () => {
+        let result = eval(inputText);
+        setInputText(result);
+    }
+    const clear = () => {
+        setInputText("");
+    }
+
+
+
+
+
+    return(
+       <div>
+        <input type="text" className="calculator-input" value={inputText}/>
+         <div className="buttons-container">
+            <button onClick={clear}>C</button>
+            <button onClick={buttonClick}>/</button>
+            <button onClick={buttonClick}>*</button>
+            <button onClick={buttonClick}>-</button>
+            
+            <button onClick={buttonClick}>7</button>
+            <button onClick={buttonClick}>8</button>
+            <button onClick={buttonClick}>9</button>
+            <button onClick={buttonClick} id="plus">+</button>
+
+            <button onClick={buttonClick}>4</button>
+            <button onClick={buttonClick}>5</button>
+            <button onClick={buttonClick}>6</button>
+
+            <button onClick={buttonClick}>1</button>
+            <button onClick={buttonClick}>2</button>
+            <button onClick={buttonClick}>3</button>
+            <button onClick={ans} id="equals">=</button>
+            <button onClick={buttonClick}>0</button>
+            <button onClick={buttonClick}>.</button>
+        </div>
+       </div>
+    )
+}
+export default App;

a97e366eb893cbdc79de90850c4c9a2954d558cd
hemant0933 <116268777+hemant0933@users.noreply.github.com>
2023-05-20 20:38:35 +0530

    submitting code-Sat May 20 2023 20:38:33 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -1,48 +1,12 @@
-import  React, {useState} from "react";
-
+import  React from "react";
+import Calculator from "./Calculator";
 const App = () => {
-    let [inputText, setInputText] = useState("");
-
-    const buttonClick = (event) => {
-        let clickedButton = event.target;
-        let value = clickedButton.innerHTML;
-        setInputText(inputText + value);
-    }
-    const ans = () => {
-        let result = eval(inputText);
-        setInputText(result);
-    }
-    const clear = () => {
-        setInputText("");
-    }
+    
     return(
-       <div className='calc-wrapper Calculator'>
-        <input type="text" id="calci" className="calculator-input" value={inputText}/>
-         <div className="buttons-container" id="btn-div">
-            <button onClick={clear}>C</button>
-            <button onClick={buttonClick}>/</button>
-            <button onClick={buttonClick}>*</button>
-            <button onClick={buttonClick}>-</button>
-            
-            <button onClick={buttonClick} id="btn-7">7</button>
-            <button onClick={buttonClick} id="btn-8">8</button>
-            <button onClick={buttonClick} id="btn-9">9</button>
-            <button onClick={buttonClick} id="plus">+</button>
-
-            <button onClick={buttonClick} id="btn-4">4</button>
-            <button onClick={buttonClick} id="btn-5">5</button>
-            <button onClick={buttonClick} id="btn-6">6</button>
-
-            <button onClick={buttonClick} id="btn-1">1</button>
-            <button onClick={buttonClick} id="btn-2">2</button>
-            <button onClick={buttonClick} id="btn-3">3</button>
-            <button onClick={ans} id="equal">=</button>
-            <button onClick={buttonClick}>0</button>
-            <button onClick={buttonClick}>.</button>
-        </div>
-       </div>
+      <Calculator/>
     )
 }
 export default App;
 
 // done
+//done2
---
+++ src/components/Calculator.js
@@ -0,0 +1,53 @@
+import React, {useState} from 'react';
+
+const Calculator = () => {
+
+    let [inputText, setInputText] = useState("");
+
+    const buttonClick = (event) => {
+        let clickedButton = event.target;
+        let value = clickedButton.innerHTML;
+        setInputText(inputText + value);
+    }
+    const ans = () => {
+       try{
+        let result = eval(inputText);
+        setInputText(result);
+       }catch{
+        setInputText("Error");
+       }
+    }
+    const clear = () => {
+        setInputText("");
+    }
+
+  return (
+    <div className='calc-wrapper Calculator'>
+    <input type="text" id="calci" className="calculator-input" value={inputText}/>
+     <div className="buttons-container" id="btn-div">
+        <button onClick={clear}>C</button>
+        <button onClick={buttonClick} id='btn-div'>/</button>
+        <button onClick={buttonClick} id='btn-*'>*</button>
+        <button onClick={buttonClick} id="btn--">-</button>
+        
+        <button onClick={buttonClick} id="btn-7">7</button>
+        <button onClick={buttonClick} id="btn-8">8</button>
+        <button onClick={buttonClick} id="btn-9">9</button>
+        <button onClick={buttonClick} id="plus">+</button>
+
+        <button onClick={buttonClick} id="btn-4">4</button>
+        <button onClick={buttonClick} id="btn-5">5</button>
+        <button onClick={buttonClick} id="btn-6">6</button>
+
+        <button onClick={buttonClick} id="btn-1">1</button>
+        <button onClick={buttonClick} id="btn-2">2</button>
+        <button onClick={buttonClick} id="btn-3">3</button>
+        <button onClick={ans} id="equal">=</button>
+        <button onClick={buttonClick}>0</button>
+        <button onClick={buttonClick}>.</button>
+    </div>
+   </div>
+  );
+}
+
+export default Calculator;
